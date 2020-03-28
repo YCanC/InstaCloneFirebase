59366c93b25012c5a68ace2d25bc6f7928d2e9fa
yasarcan <yasarcan@yasarcans-Mac.local>
2020-03-28 09:43:30 -0700

    first commit

---
+++ InstaCloneFirebase/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  InstaCloneFirebase
+//
+//  Created by yasarcan on 23.03.2020.
+//  Copyright © 2020 yasarcan. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
